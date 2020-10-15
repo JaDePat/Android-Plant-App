e1ed52f4ed460594221f0c34160a628381f5da8c
mmmz21 <emmawilcox121@gmail.com>
2020-10-15 00:42:59 -0700

    Added xml for individual plant ros

---
+++ app/src/main/java/com/example/plantapp/objects/plant.java
@@ -0,0 +1,69 @@
+package com.example.plantapp.objects;
+
+public class plant {
+    private int ID;
+    private String Name;
+    private String Scientific_Name;
+    private String Light;
+    private String Water;
+    private String Fertilizer;
+    private String Temperature;
+    private String Humidity;
+    private String Flowering;
+    private String Pests;
+    private String Diseases;
+    private String Soil;
+    private String Pot_size;
+    private String Pruning;
+    private String Propagation;
+    private String Poisonous_plant_info;
+
+    public int getID() {
+        return ID;
+    }
+    public String getName () {
+        return Name;
+    }
+    public String geScientificName () {
+        return Scientific_Name;
+    }
+    public String getLight() {
+        return Light;
+    }
+    public String getWater() {
+        return Water;
+    }
+    public String getFertilizer() {
+        return Fertilizer;
+    }
+    public String getTemperature() {
+        return Temperature;
+    }
+    public String getHumidity() {
+        return Humidity;
+    }
+    public String getFlowering() {
+        return Flowering;
+    }
+    public String getPests() {
+        return Pests;
+    }
+    public String getDiseases() {
+        return Diseases;
+    }
+    public String getSoil() {
+        return Soil;
+    }
+    public String getPotSize() {
+        return Pot_size;
+    }
+    public String getPruning() {
+        return Pruning;
+    }
+    public String getPropagation() {
+        return Propagation;
+    }
+    public String getPoisonousPlantInfo() {
+        return Poisonous_plant_info;
+    }
+}
