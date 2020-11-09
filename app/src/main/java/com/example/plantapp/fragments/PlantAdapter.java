dc9fa1aa8d4e10dd8d8ba10a2e2d9ae03fa1d52b
Misael Muñoz <munoz.misael3636@gmail.com>
2020-11-09 14:05:50 -0800

    Testing

---
+++ app/src/main/java/com/example/plantapp/fragments/PlantAdapter.java
@@ -0,0 +1,97 @@
+package com.example.plantapp.fragments;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.plantapp.R;
+import com.example.plantapp.objects.Plant;
+
+import java.util.ArrayList;
+
+public class PlantAdapter extends RecyclerView.Adapter<PlantAdapter.PlantViewHolder> implements Filterable {
+    private ArrayList<Plant> mPlantList;
+    private ArrayList<Plant> mPlantListFull;
+
+    public static class PlantViewHolder extends RecyclerView.ViewHolder {
+        public TextView mTextView1;
+        public TextView mTextView2;
+
+        public PlantViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mTextView1 = itemView.findViewById(R.id.textView);
+            mTextView2 = itemView.findViewById(R.id.textView2);
+        }
+    }
+
+    public PlantAdapter(ArrayList<Plant> plantList) {
+        mPlantList = plantList;
+        mPlantListFull = new ArrayList<>(plantList);
+    }
+
+    @NonNull
+    @Override
+    public PlantViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.plant_item,
+                parent, false);
+        PlantViewHolder pvh = new PlantViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PlantViewHolder holder, int position) {
+        Plant currentPlant = mPlantList.get(position);
+
+        holder.mTextView1.setText(currentPlant.getName());
+        holder.mTextView2.setText(currentPlant.getScientific_Name());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mPlantList.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return plantFilter;
+    }
+
+    private Filter plantFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            ArrayList<Plant> filteredList = new ArrayList<>();
+
+            if(constraint == null || constraint.length() == 0) {
+                filteredList.addAll(mPlantListFull);
+            }
+            else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+
+                for(Plant item : mPlantListFull) {
+                    if(item.getName().toLowerCase().contains(filterPattern)) {
+                        filteredList.add(item);
+                    }
+                }
+            }
+
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            mPlantList.clear();
+            mPlantList.addAll((ArrayList)results.values);
+            notifyDataSetChanged();
+        }
+    };
+}
--- app/src/main/java/com/example/plantapp/fragments/SearchFragment.java
+++ app/src/main/java/com/example/plantapp/fragments/SearchFragment.java
@@ -2,13 +2,24 @@ package com.example.plantapp.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 
 import com.example.plantapp.R;
+import com.example.plantapp.objects.Plant;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +27,9 @@ import com.example.plantapp.R;
  * create an instance of this fragment.
  */
 public class SearchFragment extends Fragment {
+    private RecyclerView mRecyclerView;
+    private PlantAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,12 +69,57 @@ public class SearchFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        setHasOptionsMenu(true);
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        ArrayList<Plant> plantList = new ArrayList<>();
+        plantList.add(new Plant(36, "Rose", "a", "b", "c", "d", "e", "f", "g", "h", "i",
+                "j", "k", "l", "m", "n"));
+        plantList.add(new Plant(33, "Rose1", "a", "b", "c", "d", "e", "f", "g", "h", "i",
+                "j", "k", "l", "m", "n"));
+        plantList.add(new Plant(38, "Rose2", "a", "b", "c", "d", "e", "f", "g", "h", "i",
+                "j", "k", "l", "m", "n"));
+        plantList.add(new Plant(39, "Rose3", "a", "b", "c", "d", "e", "f", "g", "h", "i",
+                "j", "k", "l", "m", "n"));
+        plantList.add(new Plant(31, "Rose4", "a", "b", "c", "d", "e", "f", "g", "h", "i",
+                "j", "k", "l", "m", "n"));
+
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+        mRecyclerView = view.findViewById(R.id.searchRecyclerView);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(view.getContext());
+        mAdapter = new PlantAdapter(plantList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        //return inflater.inflate(R.layout.fragment_search, container, false);
+        return view;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.plant_search, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.plant_menu_search);
+        SearchView searchView = (SearchView) searchItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
     }
 }
